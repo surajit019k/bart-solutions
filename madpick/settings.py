fb19e0eef9c7aa9e9bb887c90a256edd0c4a8206
surajit019k <129853076+surajit019k@users.noreply.github.com>
2026-05-04 20:39:10 +0530

    Update settings.py

--- madpick/settings.py
+++ madpick/settings.py
@@ -107,8 +107,9 @@ WSGI_APPLICATION = 'madpick.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
     }
 }
 
