4715bf7bcd729f2896f688de73549d0796943a07
surajit019k <129853076+surajit019k@users.noreply.github.com>
2026-05-05 15:17:24 +0530

    disable browser reload

--- madpick/settings.py
+++ madpick/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'mad_user',
     'method_handler',
-    "django_browser_reload",
+    # "django_browser_reload",
     "django_cleanup.apps.CleanupConfig",
 ]
 
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
+    # "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
