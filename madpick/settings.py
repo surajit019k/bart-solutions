99c6471e1330c53f28d7f3a82cef0127856f929d
surajit019k <129853076+surajit019k@users.noreply.github.com>
2026-05-05 15:46:58 +0530

    hosts add

--- madpick/settings.py
+++ madpick/settings.py
@@ -38,7 +38,7 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
     # Only allow your specific Render domain in production
-    ALLOWED_HOSTS = ['bart-solutions.onrender.com']
+    ALLOWED_HOSTS = ['bart-solutions.onrender.com','127.0.0.1']
 
 
 # Application definition
