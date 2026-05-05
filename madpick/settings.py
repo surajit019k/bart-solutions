afd6839435ac0058ac2c65b397db4a80d43e413f
surajit019k <129853076+surajit019k@users.noreply.github.com>
2026-05-05 22:36:20 +0530

    /static/ debug

--- madpick/settings.py
+++ madpick/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 if ENVIRONMENT == 'production':
     DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
