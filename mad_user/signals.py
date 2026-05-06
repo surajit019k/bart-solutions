d9df9d130f4aec6d88ef3c496fdd3897877142bc
surajit019k <129853076+surajit019k@users.noreply.github.com>
2026-05-06 21:20:00 +0530

    only signup and enter

--- mad_user/signals.py
+++ mad_user/signals.py
@@ -1,9 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from allauth.account.models import EmailAddress
-from allauth.account.signals import user_signed_up
-from allauth.account.adapter import get_adapter
 from .models import Profile
 from django.shortcuts import get_object_or_404
 
