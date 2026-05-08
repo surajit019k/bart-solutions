5cec520a007023259aeae029da4285a04be50d14
surajit019k <129853076+surajit019k@users.noreply.github.com>
2026-05-08 07:21:47 +0530

    success1

--- madpick/settings.py
+++ madpick/settings.py
@@ -160,9 +160,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[BASE_DIR/"static",]
 STATIC_ROOT= BASE_DIR/"staticfiles"
 
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+if ENVIRONMENT=='development':
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 LOGIN_REDIRECT_URL = '/'
